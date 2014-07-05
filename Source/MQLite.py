856c973e9ab5766384d909ee8a987bf2f5a26665
Beluki <beluki@gmx.com>
2014-07-05 19:19:56 +0200

    Add literals.

--- Source/MQLite.py
+++ Source/MQLite.py
@@ -38,6 +38,27 @@ class NoMatch(object):
 _no_match = NoMatch()
 
 
+class MatchAny(object):
+    """
+    Matches any input data.
+    """
+    def __call__(self, data):
+        return data
+
+class MatchEqual(object):
+    """
+    Matches input data that is equal to a given value.
+    """
+    def __init__(self, value):
+        self.value = value
+
+    def __call__(self, data):
+        if self.value == data:
+            return data
+        else:
+            return _no_match
+
+
 # Compiler from JSON to matchers (callables).
 
 class CompilerException(Exception):
@@ -53,6 +74,12 @@ class Compiler(object):
         """
         Compile a pattern to a matching class.
         """
+        if pattern is None:
+            return MatchAny()
+
+        if type(pattern) in [bool, int, float]:
+            return MatchEqual(pattern)
+
         raise CompilerException('Unknown datatype: %s' % pattern)
 
 
@@ -61,10 +88,10 @@ class Compiler(object):
 def main():
     compiler = Compiler()
 
-    match = compiler.compile([])
-    data = { 'a': 20, 'b': 30 }
+    match = compiler.compile(True)
+    data = True
 
-    print(match(pattern))
+    print(match(data))
 
 
 if __name__ == '__main__':
