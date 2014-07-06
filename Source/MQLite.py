6e4311bf791bb48f6b22764947ca31cb8047ab4a
Beluki <beluki@gmx.com>
2014-07-06 19:46:24 +0200

    Start working on a REPL.

--- Source/MQLite.py
+++ Source/MQLite.py
@@ -11,6 +11,8 @@ import json
 import sys
 
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
+from collections import OrderedDict
+from json import JSONDecoder
 
 
 # Information and error messages:
@@ -25,7 +27,10 @@ def errln(line):
     print(line, file = sys.stderr)
 
 
-# Matchers.
+# Matcher classes.
+
+# Matchers are callables that the compiler emits while walking the JSON nodes.
+# Each callable tests for particular data or combines other callables.
 
 class NoMatch(object):
     """
@@ -33,7 +38,6 @@ class NoMatch(object):
     Needed because None is a legitimate match.
     """
     pass
-
 
 _no_match = NoMatch()
 
@@ -48,39 +52,17 @@ class MatchAny(object):
 
 class MatchEqual(object):
     """
-    Matches input data that is equal to a given value.
+    Matches data equal to a given value.
     """
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, data):
+        self.data = data
 
     def __call__(self, data):
-        if self.value == data:
+        if self.data == data:
             return data
         else:
             return _no_match
 
-class MatchMulti(object):
-
-    def __init__(self, matcher):
-        self.matcher = matcher
-
-    def __call__(self, data):
-        if isinstance(data, list):
-            result = []
-
-            for value in data:
-                current = self.matcher(value)
-
-                if not current is _no_match:
-                    result.append(current)
-
-            if len(result) == 0:
-                return _no_match
-
-            return result
-
-        else:
-            return self.matcher(data)
 
 class MatchEmptyDict(object):
     """
@@ -117,7 +99,7 @@ class MatchDict(object):
     The result is a dict containing all the matching keys/values.
     """
     def __init__(self, compiled_dict):
-        self.compiled_dict = compiled_dict
+        self.compiled_dict = list(compiled_dict.items())
 
     def __call__(self, data):
 
@@ -126,9 +108,9 @@ class MatchDict(object):
             return _no_match
 
         result = {}
-        for key, matcher in self.compiled_dict.items():
 
-            # key in compiled dict not in data?
+        for key, matcher in self.compiled_dict:
+
             if not key in data:
                 return _no_match
 
@@ -157,7 +139,7 @@ class MatchList(object):
     The result is a list containing all the matches.
     """
     def __init__(self, compiled_list):
-        self.list = compiled_list
+        self.compiled_list = compiled_list
 
     def __call__(self, data):
 
@@ -166,7 +148,7 @@ class MatchList(object):
             return _no_match
 
         result = []
-        for matcher in self.list:
+        for matcher in self.compiled_list:
             matched = False
 
             for value in data:
@@ -181,6 +163,34 @@ class MatchList(object):
                 return _no_match
 
         return result
+
+
+class MatchToLists(object):
+    """
+    Wraps a matching class so that when used against a list
+    it tries to match every element and return the matching results.
+    The match succeeds if any of the list elements matches.
+    """
+    def __init__(self, matcher):
+        self.matcher = matcher
+
+    def __call__(self, data):
+        if isinstance(data, list):
+            result = []
+
+            for value in data:
+                current = self.matcher(value)
+
+                if not current is _no_match:
+                    result.append(current)
+
+            if len(result) == 0:
+                return _no_match
+
+            return result
+
+        else:
+            return self.matcher(data)
 
 
 # Compiler from JSON to matchers (callables).
@@ -202,10 +212,10 @@ class Compiler(object):
             return self.compile_none(pattern)
 
         if type(pattern) in [bool, int, float, str]:
-            return MatchMulti(self.compile_literal(pattern))
+            return MatchToLists(self.compile_literal(pattern))
 
         if isinstance(pattern, dict):
-            return MatchMulti(self.compile_dict(pattern))
+            return MatchToLists(self.compile_dict(pattern))
 
         if isinstance(pattern, list):
             return self.compile_list(pattern)
@@ -251,17 +261,107 @@ class Compiler(object):
         return MatchList(compiled_list)
 
 
+# Parser:
+
+def make_parser():
+    parser = ArgumentParser(
+        description = __doc__,
+        formatter_class = RawDescriptionHelpFormatter,
+        epilog = 'example: MQLite.py [file.json]',
+        usage  = 'MQLite.py [filepath]')
+
+    # optional:
+    parser.add_argument('filepath',
+        help = 'run a repl with filepath as the JSON data',
+        metavar = 'filepath', nargs = '?')
+
+    return parser
+
+
+# REPL:
+
+class REPL(object):
+
+    def __init__(self, filepath):
+        self.compiler = Compiler()
+        self.decoder = JSONDecoder(object_pairs_hook = OrderedDict)
+        self.filepath = filepath
+        self.data = None
+        self.prompt = '>>>'
+        self.indent = 2
+
+        self.commands = {
+            'load': self.load_data,
+        }
+
+    def load_data(self, filepath):
+        """
+        Change the current file to pattern match against.
+        """
+        try:
+            with open(filepath) as descriptor:
+                self.data = self.decoder.decode(descriptor.read())
+                self.filepath = filepath
+
+        except Exception as err:
+            errln('Unable to load: %s - %s' % (filepath, str(err)))
+
+    def execute_metacommand(self, text):
+        """
+        """
+        parts = text.split(maxsplit = 1)
+
+        command = parts[0]
+        parameters = parts[1:]
+
+        if command in self.commands:
+            self.commands[command](*parameters)
+
+    def execute_pattern(self, pattern):
+        """
+        Parse/compile a pattern and run it against our data.
+        """
+        if self.data is None:
+            errln('No data loaded.')
+            return
+
+        try:
+            parsed = json.loads(pattern)
+            compiled = self.compiler.compile(parsed)
+
+            result = compiled(self.data)
+
+            if result is _no_match:
+                outln('No results.')
+            else:
+                outln(json.dumps(result, indent = self.indent))
+
+        except Exception as err:
+            errln('Unable to execute: %s' % str(err))
+
+    def run(self):
+        """
+        Run an interactive read-eval-print-loop.
+        """
+        self.load_data(self.filepath)
+
+        while True:
+            text = input(str(self.filepath) + " " + str(self.prompt) + " ")
+
+            if text.startswith(','):
+                command = text[1:]
+                self.execute_metacommand(command)
+            else:
+                self.execute_pattern(text)
+
+
 # Entry point:
 
 def main():
-    compiler = Compiler()
+    parser = make_parser()
+    options = parser.parse_args()
 
-    data = [1, 2, 1]
-    pattern = None
-
-    match = compiler.compile(pattern)
-
-    print(match(data))
+    REPL(options.filepath).run()
 
 
 if __name__ == '__main__':
