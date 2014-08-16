7df19cfcd60f4acf4fe14c830fa3374e257a5d42
Beluki <beluki@gmx.com>
2014-08-16 19:30:35 +0200

    Added setup.py for setuptools.

---
+++ Source/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name = 'MQLite',
+    version = '2014.08.16',
+    url = 'https://github.com/Beluki/MQLite',
+    license = 'See Documentation/License',
+    author = 'Beluki',
+    author_email = 'beluki@gmx.com',
+    description = 'Pattern match JSON like you query Freebase, using a simple MQL dialect.',
+    py_modules = ['MQLite'],
+    zip_safe = False,
+    platforms = 'any',
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Code Generators',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ]
+)
+
